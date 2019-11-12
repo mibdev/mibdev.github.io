e8109d4472c55a129a506042ea2b63993de45811
Bruno Silva <bruno3dcontato@gmail.com>
2019-11-12 01:41:37 -0300

    Added more details to About page

---
+++ build/precache-manifest.69834e3e1ae449dfeff515c180e7d49e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54166113759334b478d10f4a8730adbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "939d66d9111bc3e250c9",
+    "url": "/static/js/2.7de06f37.chunk.js"
+  },
+  {
+    "revision": "4409aa76764d9b683c0e",
+    "url": "/static/js/main.a50111a8.chunk.js"
+  },
+  {
+    "revision": "b7ea830f15ecdd980b46",
+    "url": "/static/js/runtime-main.b4250194.js"
+  },
+  {
+    "revision": "073b4d5a9995ac23a945b3b0c6d99649",
+    "url": "/static/media/mib_logo_dark.073b4d5a.svg"
+  },
+  {
+    "revision": "9e58fcacdd18345963edc2454fb6a203",
+    "url": "/static/media/poly.9e58fcac.svg"
+  }
+]);
