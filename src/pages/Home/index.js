229e376a6fcfa5b30324a183939be5653f8e5eed
Bruno Silva <bruno3dcontato@gmail.com>
2019-11-16 14:56:49 -0300

    4tgw654g

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 import { Container } from './styles';
 
-import oldPC from '../../assets/old-pc.png';
-
 export default function Home() {
     return (
         <Container>
             <div className="old-pc">
-                <img src={oldPC} alt="old-pc" />
+                <img src={`${process.env.PUBLIC_URL}/old-pc.png`} alt="old-pc" />
             </div>
             <div className="animated-container">
                 <h1>A bunch of <span>friends</span> trying to make cool things</h1>
--- src/pages/Team/index.js
+++ src/pages/Team/index.js
@@ -9,9 +9,9 @@ export default function Team() {
     return (
         <Container>
             <CardList>
-                <Card image="/bruno.jpg" title="Bruno Silva" description="Nothing to see." />
-                <Card image="/caio.jpg" title="Caio Domingues" description="Caio is passionate about Open Source world, feels like in a mission to help the world runs better." />
-                <Card image="/samuel.jpg" title="Samuel Schultze" description="I don't know what to type about Samuel because he looks like a bad boy, so, if I type something cute, probably will not be like him." />
+                <Card image={`${process.env.PUBLIC_URL}/bruno.jpg`} title="Bruno Silva" description="Nothing to see." />
+                <Card image={`${process.env.PUBLIC_URL}/caio.jpg`} title="Caio Domingues" description="Caio is passionate about Open Source world, feels like in a mission to help the world runs better." />
+                <Card image={`${process.env.PUBLIC_URL}/samuel.jpg`} title="Samuel Schultze" description="I don't know what to type about Samuel because he looks like a bad boy, so, if I type something cute, probably will not be like him." />
             </CardList>
         </Container>
     );
