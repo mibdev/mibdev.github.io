bdba219bdf41b3fd30da047f692170ae2959740c
Bruno Silva <bruno3dcontato@gmail.com>
2019-11-16 14:21:33 -0300

    image cdn error, deploy test

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -9,12 +9,12 @@ export default function Projects() {
     return (
         <Container>
             <CardList>
-                <Card image="/hackatomic-social.png" title="Hackatomic" description="Conecte-se com pessoas ao redor do mundo e crie enquanto corre contra o tempo, conheça novas pessoas e divirta-se! E o melhor: é tudo de graça." />
-                <Card image="/hackatomic-social.png" title="Hackatomic" description="Conecte-se com pessoas ao redor do mundo e crie enquanto corre contra o tempo, conheça novas pessoas e divirta-se! E o melhor: é tudo de graça." />
-                <Card image="/hackatomic-social.png" title="Hackatomic" description="Conecte-se com pessoas ao redor do mundo e crie enquanto corre contra o tempo, conheça novas pessoas e divirta-se! E o melhor: é tudo de graça." />
-                <Card image="/hackatomic-social.png" title="Hackatomic" description="Conecte-se com pessoas ao redor do mundo e crie enquanto corre contra o tempo, conheça novas pessoas e divirta-se! E o melhor: é tudo de graça." />
-                <Card image="/hackatomic-social.png" title="Hackatomic" description="Conecte-se com pessoas ao redor do mundo e crie enquanto corre contra o tempo, conheça novas pessoas e divirta-se! E o melhor: é tudo de graça." />
-                <Card image="/hackatomic-social.png" title="Hackatomic" description="Conecte-se com pessoas ao redor do mundo e crie enquanto corre contra o tempo, conheça novas pessoas e divirta-se! E o melhor: é tudo de graça." />
+                <Card image="/hackatomic.jpg" title="Hackatomic" description="Conecte-se com pessoas ao redor do mundo e crie enquanto corre contra o tempo, conheça novas pessoas e divirta-se! E o melhor: é tudo de graça." />
+                <Card image="/hackatomic.jpg" title="Hackatomic" description="Conecte-se com pessoas ao redor do mundo e crie enquanto corre contra o tempo, conheça novas pessoas e divirta-se! E o melhor: é tudo de graça." />
+                <Card image="/hackatomic.jpg" title="Hackatomic" description="Conecte-se com pessoas ao redor do mundo e crie enquanto corre contra o tempo, conheça novas pessoas e divirta-se! E o melhor: é tudo de graça." />
+                <Card image="/hackatomic.jpg" title="Hackatomic" description="Conecte-se com pessoas ao redor do mundo e crie enquanto corre contra o tempo, conheça novas pessoas e divirta-se! E o melhor: é tudo de graça." />
+                <Card image="/hackatomic.jpg" title="Hackatomic" description="Conecte-se com pessoas ao redor do mundo e crie enquanto corre contra o tempo, conheça novas pessoas e divirta-se! E o melhor: é tudo de graça." />
+                <Card image="/hackatomic.jpg" title="Hackatomic" description="Conecte-se com pessoas ao redor do mundo e crie enquanto corre contra o tempo, conheça novas pessoas e divirta-se! E o melhor: é tudo de graça." />
             </CardList>
         </Container>
     );
