f9ffb11eebec2343c05e47465a4b4ebb37a670af
Bruno Silva <bruno3dcontato@gmail.com>
2019-11-17 13:28:28 -0300

    contact page created

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,11 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Container } from './styles';
 
 export default function Contact() {
+
+    const [inputName, setInputName] = useState("");
+    const [inputMail, setInputMail] = useState("");
+    const [inputMessage, setInputMessage] = useState("");
+
+    function handleNameChange(event) {
+        setInputName(event.target.value);
+    }
+
+    function handleMailChange(event) {
+        setInputMail(event.target.value);
+    }
+
+    function handleMessageChange(event) {
+        setInputMessage(event.target.value);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        const body = {
+            content: `**NAME**: ${inputName}\n**MAIL**: ${inputMail}\n**MESSAGE**: ${inputMessage}`,
+            embeds: [
+                {
+                    title: inputName,
+                    description: inputMessage,
+                    author: {
+                        name: inputMail,
+                    },
+                    color: 16731983
+                }
+            ]
+        };
+
+        const xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("POST", "https://discordapp.com/api/webhooks/643862830168145920/HcLRUH4Bh8RBj-z340Shr53-lICFl24-SGnkaZdGvvuoJQXYxmjhdOrM8Mi3D9WOD1u5");
+        xmlhttp.setRequestHeader("Content-Type", "application/json");
+        xmlhttp.send(JSON.stringify(body));
+    }
+
     return (
         <Container>
-            <h1>Contact</h1>
+            <div className="row">
+                <div className="right-content">
+                    <div className="animated-container">
+                        <h1>Leave your <span>message</span></h1>
+                    </div>
+                    <img src={`${process.env.PUBLIC_URL}/telephone.png`} alt="telephone" />
+                </div>
+                <form onSubmit={handleSubmit}>
+                    <div class="textbox">
+                        <label for="name">Nome</label>
+                        <input
+                            required
+                            type="text"
+                            value={inputName}
+                            onChange={handleNameChange}
+                        />
+                    </div>
+
+                    <div class="textbox">
+                        <label for="mail">E-mail</label>
+                        <input
+                            required
+                            type="email"
+                            value={inputMail}
+                            onChange={handleMailChange}
+                        />
+                    </div>
+
+                    <div class="textbox">
+                        <label for="msg">Mensagem</label>
+                        <div className="textarea-container">
+                            <textarea
+                                required
+                                maxlength={1500}
+                                value={inputMessage}
+                                onChange={handleMessageChange}
+                            />
+                            <span className="char-counter">{`${inputMessage.length} - 1500`}</span>
+                        </div>
+                    </div>
+
+                    <div class="submit-button">
+                        <button type="submit">Enviar mensagem!</button>
+                    </div>
+                </form>
+            </div>
         </Container>
     );
 }
--- src/pages/Contact/styles.js
+++ src/pages/Contact/styles.js
@@ -1,4 +1,7 @@
 import styled from 'styled-components';
+
+import { FlexColumnCenter } from '../../styles/FlexColumnCenter';
+import { AnimationGrayscaleOut } from '../../styles/AnimationGrayscaleOut';
 import { AnimationSlideInFromBottom } from '../../styles/AnimationSlideInFromBottom';
 
 export const Container = styled.div`
@@ -11,6 +14,172 @@ export const Container = styled.div`
         justify-content: center;
 
         ${AnimationSlideInFromBottom}
+    }
+
+    &>.row {
+        width: 100%;
+
+        display: flex;
+        flex-direction: row;
+    }
+
+    & .right-content>.animated-container {
+        margin-bottom: 1rem;
+
+        ${AnimationSlideInFromBottom}
+        animation-delay: 0.3s;
+
+        &>h1 {
+            transform: rotate(-5deg);
+
+            font-size: 2.25rem;
+            font-family: 'Permanent Marker', cursive;
+        }
+        &>h1>span {
+            color: #000;
+            background: #fff;
+
+            padding: 0 1rem;
+    
+            font-size: 2.25rem;
+            font-family: 'Permanent Marker', cursive;
+        }
+    }
+    
+    & .right-content>img {
+        width: 25rem;
+
+        ${AnimationGrayscaleOut}
+        animation-duration: 0.5s;
+        animation-delay: 1s;
+    }
+
+    & form {
+        width: 50%;
+        margin: 0 15rem;
+        ${FlexColumnCenter}
+    }
+
+    & form>.textbox {
+        width: 100%;
+        margin-top: 1.25rem;
+
+        ${FlexColumnCenter}
+        align-items: flex-start;
+    }
+
+    & form>.submit-button {
+        width: 100%;
+        margin-top: 1.25rem;
+
+        ${FlexColumnCenter}
+    }
+
+    & form label {
+        text-transform: uppercase;
+    }
+    
+    & form input {
+        width: 100%;
+        
+        padding: 0.45rem;
+        margin-top: 0.25rem;
+
+        font-size: 1rem;
+
+        outline: none;
+
+        background: #0C0C0C;
+        border: 1px solid #fff;
+        box-shadow: -0.3rem 0.35rem 0 #fff;
+
+        transition: all 0.2s;
+
+        &:focus, &:hover {
+            box-shadow: 0 0 0 #fff;
+        }
+    }
+    
+    & form .textarea-container {
+        position: relative;
+
+        width: 100%;
+    }
+
+    & form .textarea-container>textarea {
+        min-width: 100%;
+        max-width: 100%;
+        min-height: 7rem;
+
+        padding: 0.45rem;
+        margin-top: 0.1rem;
+
+        font-size: 1rem;
+        
+        outline: none;
+
+        background: #0C0C0C;
+        border: 1px solid #fff;
+
+        box-shadow: -0.3rem 0.35rem 0 #fff;
+
+        transition: all 0.2s;
+
+        &:focus, &:hover {
+            box-shadow: 0 0 0 #fff;
+        }
+    }
+
+    /* Scrollbar */
+   & form .textarea-container>textarea::-webkit-scrollbar {
+        cursor: pointer;
+       width: 0.75rem;
+    }
+
+    /* Track */
+   & form .textarea-container>textarea::-webkit-scrollbar-track {
+        background: #fff; 
+    }
+
+    /* Handle */
+   & form .textarea-container>textarea::-webkit-scrollbar-thumb {
+        background: #333; 
+    }
+
+    /* Handle on hover */
+   & form .textarea-container>textarea::-webkit-scrollbar-thumb:hover {
+        background: #555; 
+    }
+
+    & form .textarea-container>.char-counter {
+        position: absolute;
+
+        right: -0.2rem;
+        bottom: 0.5rem;
+
+        font-size: 0.75rem;
+
+        cursor: default;
+        user-select: none;
+    }
+
+    & form button {
+        padding: 0.45rem 1rem;
+
+        font-size: 1rem;
+
+        outline: none;
+        cursor: pointer;
+
+        background: #0C0C0C;
+        border: 1px solid #fff;
+        box-shadow: -0.3rem 0.35rem 0 #fff;
+
+        transition: all 0.2s;
+
+        &:hover {
+            box-shadow: 0 0 0 #fff;
+        }
     }
 
 `;
