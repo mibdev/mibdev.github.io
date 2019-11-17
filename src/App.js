4d36815487944a0172b12c90b6559f6a9626e334
Bruno Silva <bruno3dcontato@gmail.com>
2019-11-17 17:07:55 -0300

    main route change

--- src/App.js
+++ src/App.js
@@ -20,13 +20,13 @@ export default function App() {
                 <BrowserRouter>
                     <Menu />
                     <Switch>
-                        <Route path="/mib-dev" exact component={Home} />
+                        <Route path="/" exact component={Home} />
                         <Route path="/about" component={About} />
                         <Route path="/projects" component={Projects} />
                         <Route path="/team" component={Team} />
                         <Route path="/contact" component={Contact} />
 
-                        <Redirect from="*" to="/mib-dev" />
+                        <Redirect from="*" to="/" />
                     </Switch>
                 </BrowserRouter>
             </div>
