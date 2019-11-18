d13e4b169de92abe88623dd1496af7b5193704f1
Bruno Silva <bruno3dcontato@gmail.com>
2019-11-18 05:59:57 -0300

    Fixed: home aways active bug

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 import { Container } from './styles';
 
-export default function Menu({ location }) {
+export default function Menu() {
 
     return (
         <Container>
@@ -11,7 +11,7 @@ export default function Menu({ location }) {
                 <a className="logo" href="/">Mib</a>
             </h1>
             <div className="nav-items">
-                <NavLink className="nav-link" activeClassName="active-nav-link" to="/">Início</NavLink>
+                <NavLink className="nav-link" activeClassName="active-nav-link" to="/" exact>Início</NavLink>
                 <NavLink className="nav-link" activeClassName="active-nav-link" to="/about">Sobre</NavLink>
                 <NavLink className="nav-link" activeClassName="active-nav-link" to="/projects">Projetos</NavLink>
                 <NavLink className="nav-link" activeClassName="active-nav-link" to="/team">Time</NavLink>
