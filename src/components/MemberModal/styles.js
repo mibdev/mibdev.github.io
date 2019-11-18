f57c51dd75b420ecf452583f663396edc9d8f031
Bruno Silva <bruno3dcontato@gmail.com>
2019-11-18 08:42:23 -0300

    warnings fix

--- src/components/MemberModal/styles.js
+++ src/components/MemberModal/styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-import { FlexRowCenter } from '../../styles/FlexRowCenter';
+// import { FlexRowCenter } from '../../styles/FlexRowCenter';
 import { FlexColumnCenter } from '../../styles/FlexColumnCenter';
 
 export const Container = styled.div`
