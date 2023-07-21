532e20c572b12b5f231642859fa9d538c50a901d
Muka Schultze <samuelschultze@gmail.com>
2023-07-21 14:16:18 -0300

    Updates

---
+++ precache-manifest.b14cef318b066beff86875656eccb981.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b05762a84fe51e9e3c8f52f1bc9b7c6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac0989b9dbb298082b30",
+    "url": "/static/js/2.2f84b09e.chunk.js"
+  },
+  {
+    "revision": "2e31c6b1729a8cf9f5db",
+    "url": "/static/js/main.50a3a74c.chunk.js"
+  },
+  {
+    "revision": "7735d4de317acf3c51bd",
+    "url": "/static/js/runtime-main.6183c8d4.js"
+  }
+]);
