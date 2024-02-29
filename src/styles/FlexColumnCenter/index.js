a75909cc9d85e1892032b33e62789dc016363a74
Gabriel de Jesus <devgabrieldejesus@gmail.com>
2024-02-29 19:57:07 -0300

    feat: delete unnecessary files and code

--- src/styles/FlexColumnCenter/index.js
+++
@@ -1,9 +0,0 @@
-import { css } from 'styled-components';
-
-export const FlexColumnCenter = css`
-        display: flex;
-        flex-direction: column;
-
-        align-items: center;
-        justify-content: center;
-`;
--- src/styles/FlexRowCenter/index.js
+++
@@ -1,9 +0,0 @@
-import { css } from 'styled-components';
-
-export const FlexRowCenter = css`
-        display: flex;
-        flex-direction: row;
-
-        align-items: center;
-        justify-content: center;
-`;
